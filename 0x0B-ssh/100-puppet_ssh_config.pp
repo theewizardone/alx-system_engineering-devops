662410ba25524e602610b7d4e87bb18e6d5a0171
Alfonce Morara <alfoncemorara412@gmail.com>
2024-01-25 14:42:32 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
+# Seting up my client config file
+include stdlib
 
-#using puppet to connect without password 
-file { '/etc/ssh/ssh_config':
-	ensure =present,
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line {'Turn off wifi passwd auth':
-	path  =>'/etc/ssh/ssh_config
-	line  =>'passwordAuthentication no;
-	match => /'#passwordAuthentication;
-}
-
-file_line{Declare identity file:
-	path =>'/etc/ssh/ssh_config;
-	line =>'IdentityFile ~/.ssh/school',
-	match  =>'^IdentityFile',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
